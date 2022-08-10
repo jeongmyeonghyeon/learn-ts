797fc928a3b2c8035085627c9c4847bb09d6e627
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-10 17:56:23 +0900

    구체적으로 작성한 중복된  타입 정의를 interface로 정의

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,21 @@
-let todoItems: { id: number; title: string; done: boolean }[];
+// let todoItems: { id: number; title: string; done: boolean }[];
+
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// };
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -16,7 +30,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: { id: number; title: string; done: boolean }): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -24,10 +38,7 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(
-  index: number,
-  todo: { id: number; title: string; done: boolean }
-): void {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -42,11 +53,10 @@ function showCompleted(): object[] {
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems(todosParam: [object, object]): void {
+function addTwoTodoItems(): void {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
-  todosParam.map(item => {
-    addTodo(item);
-  });
+  addTodo({ id: 4, title: 'type', done: false });
+  addTodo({ id: 5, title: 'script', done: false });
 }
 
 // NOTE: 유틸 함수
@@ -55,8 +65,5 @@ function log(): void {
 }
 
 todoItems = fetchTodoItems();
-addTwoTodoItems([
-  { id: 4, title: 'type', done: false },
-  { id: 5, title: 'script', done: false },
-]);
+addTwoTodoItems();
 log();
