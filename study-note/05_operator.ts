c9e51d2fe0cf006ace38b4e0ac252fea6d6df7f9
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-10 20:15:12 +0900

    유니온 타입 장점

--- study-note/05_operator.ts
+++ study-note/05_operator.ts
@@ -1,11 +1,20 @@
 // function logMessage(value: any) {
 //   console.log(value);
+//   // value. (추론하지 못함. 객체 타입에 따른 api를 지원하지 못함)
 // }
 // logMessage("hello");
 // logMessage(100);
 
+var seho: string | number | boolean;
 function logMessage(value: string | number) {
   console.log(value);
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError("value must be string or number");
 }
 logMessage("hello");
 logMessage(100);
