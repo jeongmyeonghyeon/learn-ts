a2e77ec57ce33f45adeed46f69aab0754a1c72e2
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-10 16:39:24 +0900

    함수의 옵셔널 파라미터

--- study-note/02_functions.ts
+++ study-note/02_functions.ts
@@ -15,3 +15,9 @@ function sum(a: number, b: number): number {
 }
 
 // sum(10, 20, 30, 40);
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {}
+
+log("hello world");
+log("hello ts", "abc");
