ee710e324b664829e5d09ab3f8dd6f197bd7d42f
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-10 20:21:35 +0900

    유니온 타입 특징 - 타입 별칭을 사용할 경우, 공통된 속성에만 접근할 수 있다.

--- study-note/05_operator.ts
+++ study-note/05_operator.ts
@@ -9,6 +9,7 @@ var seho: string | number | boolean;
 function logMessage(value: string | number) {
   console.log(value);
   if (typeof value === "number") {
+    // Type Guard
     value.toLocaleString();
   }
   if (typeof value === "string") {
@@ -18,3 +19,17 @@ function logMessage(value: string | number) {
 }
 logMessage("hello");
 logMessage(100);
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+interface Person {
+  name: string;
+  age: string;
+}
+function askSomeone(someone: Developer | Person) {
+  someone.name; // 공통된 특성에만 접근할 수 있음.
+  someone.skill; // 만약 접근하고 싶으면 위에 사용했떤 Type Guard 를 사용하면 됨.
+  someone.age;
+}
