d4a35aaf3f803ac1cd8c35e66fd4afeff1d26c45
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-10 22:49:52 +0900

    enum 활용

--- study-note/06_enum.ts
+++ study-note/06_enum.ts
@@ -13,3 +13,39 @@ enum Shoes {
 
 var myShoes = Shoes.Nike;
 console.log(myShoes); // 나이키
+
+// 이넘 활용 사례
+
+// (1) 일반 문자열
+// function askQuestion(answer: string) {
+//   if (answer === "yes") {
+//     console.log("정답입니다.");
+//   }
+//   if (answer === "no") {
+//     console.log("오답입니다.");
+//   }
+// }
+// askQuestion("예스");
+// askQuestion("y");
+// askQuestion("Yes");
+
+// (2) enu 활용
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log("정답입니다.");
+  }
+  if (answer === Answer.No) {
+    console.log("오답입니다.");
+  }
+}
+askQuestion(Answer.Yes);
+// askQuestion("Yes"); // error
+// askQuestion("예스");
+// askQuestion("y");
+
+// -> dropdown 같은 곳에서 활용...
