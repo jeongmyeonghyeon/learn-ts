6cce544d744c5bc4e5822882c3fb90234b68e05c
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-10 18:05:49 +0900

    함수의 인자에 인터페이스 적용(정말 많이 사용할 것)

--- study-note/03_interface.ts
+++ study-note/03_interface.ts
@@ -3,7 +3,18 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 var seho: User = {
   age: 33,
   name: "세호",
 };
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+const hulk = {
+  name: "헐크",
+  age: 46,
+};
+getUser(hulk);
