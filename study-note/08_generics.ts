7591eaf5037745e60b403fc4763aabeb9088bfcd
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-11 14:04:25 +0900

    기존 문법과 제네릭의 차이점 - 유니온 타입을 이용한 선언 방식의 문제점

--- study-note/08_generics.ts
+++ study-note/08_generics.ts
@@ -14,14 +14,23 @@
 // logText<string>("하이");
 
 // 기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언의 단점
-function logText(text: string) {
+// function logText(text: string) {
+//   console.log(text);
+//   return text;
+// }
+// function logNumber(number: number) {
+//   console.log(number);
+//   return number;
+// }
+// logText("a");
+// logText(10);
+// logNumber(10);
+
+// 기존 문법과 제네릭의 차이점 - 유니온 타입을 이용한 선언 방식의 문제점
+// 파라미터는 해결됐지만, 반환값에 대한 해결이 이루어지지 않았음.
+function logText(text: string | number) {
   console.log(text);
   return text;
 }
-function logNumber(number: number) {
-  console.log(number);
-  return number;
-}
-logText("a");
+const a = logText("a");
 logText(10);
-logNumber(10);
