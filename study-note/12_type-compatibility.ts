d01b1b0c3d6423fc1037376f8cbb079c681b8bb8
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-12 00:51:15 +0900

    타입 호환 - 함수, 제네릭

--- study-note/12_type-compatibility.ts
+++ study-note/12_type-compatibility.ts
@@ -16,3 +16,26 @@ var person: Person;
 // developer = person;
 // person = developer;
 developer = new Person();
+
+// 함수
+var add = function (a: number) {};
+
+var sum = function (a: number, b: number) {};
+
+// add = sum;
+sum = add;
+
+// 제네릭
+interface Empty<T> {}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+// empty1 = empty2;
+// empty2 = empty1;
+
+interface NotEmpty<T> {
+  data: T;
+}
+var notEmpty1: NotEmpty<string>;
+var notEmpty2: NotEmpty<number>;
+// notEmpty1 = notEmpty2;
+notEmpty2 = notEmpty1;
