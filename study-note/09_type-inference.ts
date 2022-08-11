2f3599fdf933676dc98ea94fb1369e5d9af98fd1
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-11 19:18:26 +0900

    인터페이스와 제네릭을 이용한 타입 추론 방식

--- study-note/09_type-inference.ts
+++ study-note/09_type-inference.ts
@@ -1,3 +1,4 @@
+// 타입 추론 기본 1
 var a = 10;
 
 function getB(b = 20) {
@@ -6,3 +7,13 @@ function getB(b = 20) {
 }
 
 20 + "30"; // 2030
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+var shoppingItem: Dropdown<string> = {
+  value: "abc",
+  title: "hello",
+};
