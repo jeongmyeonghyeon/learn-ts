3307039bb5ade539da5cf3965c9b2279648d2476
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-11 15:01:17 +0900

    제네릭 타입 제한 2 - 정의된 타입 이용하기

--- study-note/08_generics.ts
+++ study-note/08_generics.ts
@@ -61,12 +61,24 @@ const login = logText<boolean>(true);
 // const obj: Dropdown<number> = { value: "10", selected: false };
 
 // 제네릭의 타입 제한
-function logTextLength<T>(text: T[]): T[] {
-  // 타입 힌트를 줄 수 있다.
-  console.log(text.length);
-  text.forEach(function (text) {
-    console.log(text);
-  });
+// function logTextLength<T>(text: T[]): T[] {
+//   // 타입 힌트를 줄 수 있다.
+//   console.log(text.length);
+//   text.forEach(function (text) {
+//     console.log(text);
+//   });
+//   return text;
+// }
+// logTextLength<string>(["hi", "ts"]);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
   return text;
 }
-logTextLength<string>(["hi", "ts"]);
+logTextLength(10);
+logTextLength({ length: 10 });
+logTextLength({ lengh: 10 });
