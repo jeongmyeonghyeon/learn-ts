9b83ef9570f9404d05f222b9959ccd69afbf1e02
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-11 14:00:24 +0900

    제네릭 - 기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언의 단점

--- study-note/08_generics.ts
+++ study-note/08_generics.ts
@@ -6,8 +6,22 @@
 // logText("하이"); // 문자열 하이
 // logText(true); // 진위값 true
 
-function logText<T>(text: T): T {
+// 제네릭 기본 문법
+// function logText<T>(text: T): T {
+//   console.log(text);
+//   return text;
+// }
+// logText<string>("하이");
+
+// 기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복 선언의 단점
+function logText(text: string) {
   console.log(text);
   return text;
 }
-logText<string>("하이");
+function logNumber(number: number) {
+  console.log(number);
+  return number;
+}
+logText("a");
+logText(10);
+logNumber(10);
